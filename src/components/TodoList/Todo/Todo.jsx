6dda55aca4ebdc78d2d41e8029ac6d94c269af56
hs-1891014-minseokim <55376275+minseokiim@users.noreply.github.com>
2022-11-14 20:13:54 +0900

    Update Todo.jsx

--- src/components/TodoList/Todo/Todo.jsx
+++ src/components/TodoList/Todo/Todo.jsx
@@ -1,43 +1,50 @@
 import { useState, useRef } from 'react';
 import { FaTrashAlt, FaEdit } from 'react-icons/fa';
-import styles from './Todo.css';
+import './Todo.css';
 
 export default function Todo({ todo, onUpdate, onDelete, date }) {
-	const { id, text, status } = todo;
-	const [isEdit, setEdit] = useState(false);
-	const editInput = useRef();
+  const { id, text, status } = todo;
+  const [isEdit, setEdit] = useState(false);
+  const editInput = useRef();
 
-	const handleChange = (e) => {
-		const status = e.target.checked ? 'completed' : 'active';
-		onUpdate({ ...todo, status });
-	};
-	const handleDelete = () => onDelete(todo);
-	const handleEdit = () => {
-		setEdit(() => !isEdit);
-		if (isEdit === true) {
-			const text = editInput.current.value;
-			onUpdate({ ...todo, text });
-		}
-	};
+  const handleChange = (e) => {
+    const status = e.target.checked ? 'completed' : 'active';
+    onUpdate({ ...todo, status });
+  };
+  const handleDelete = () => onDelete(todo);
+  const handleEdit = () => {
+    setEdit(() => !isEdit);
+    if (isEdit === true) {
+      const text = editInput.current.value;
+      onUpdate({ ...todo, text });
+    }
+  };
 
-	return (
-		<li className={styles.todo}>
-			<input className={styles.checkbox} type='checkbox' id={id} date={date.getDate()} checked={status === 'completed'} onChange={handleChange} />
-			{isEdit ? (
-				<input ref={editInput} type='text' id={id} defaultValue={text} />
-			) : (
-				<label className={styles.text} htmlFor={id}>
-					{text}
-				</label>
-			)}
-			<span className={styles.icon}>
-				<button className={styles.button} onClick={handleEdit}>
-					<FaEdit />
-				</button>
-				<button className={styles.button} onClick={handleDelete}>
-					<FaTrashAlt />
-				</button>
-			</span>
-		</li>
-	);
+  return (
+    <li className="todo">
+      <input
+        className="checkbox"
+        type="checkbox"
+        id={id}
+        date={date.getDate()}
+        checked={status === 'completed'}
+        onChange={handleChange}
+      />
+      {isEdit ? (
+        <input ref={editInput} type="text" id={id} defaultValue={text} />
+      ) : (
+        <label className="text" htmlFor={id}>
+          {text}
+        </label>
+      )}
+      <span className="icon">
+        <button className="button" onClick={handleEdit}>
+          <FaEdit />
+        </button>
+        <button className="button" onClick={handleDelete}>
+          <FaTrashAlt />
+        </button>
+      </span>
+    </li>
+  );
 }
