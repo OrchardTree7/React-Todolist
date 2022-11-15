74e27af56a8dc2781e9a2920cea8d138d12bea6d
sunwonga <sunwonga@gmail.com>
2022-11-15 19:38:29 +0900

    Add Popup to Map

--- src/components/Map/MainMap.jsx
+++ src/components/Map/MainMap.jsx
@@ -15,9 +15,9 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 
 const MainMap = () => {
 	// 검색한 위치 정보 담긴 Array
-	const [pois, setPois] = useState([{ geometry: { coordinates: [127, 37] } }]);
+	const [pois, setPois] = useState(null);
 	// 클릭한 Marker의 정보를 확인하기 위한 state
-	const [selectedPoi, setSelectedPoi] = useState(null);
+	const [popupInfo, setPopupInfo] = useState(null);
 
 	// API 작동 확인용 위치 검색 함수
 	const onSearch = async (e) => {
@@ -30,8 +30,9 @@ const MainMap = () => {
 	};
 
 	// 마커 클릭 했을 시 Popup 출력하게 state 변경
-	const onClickMarker = (longitude, latitude) => {
-		setSelectedPoi({ geometry: { coordinates: [longitude, latitude] } });
+	const onClickMarker = (e, poi) => {
+		e.originalEvent.stopPropagation();
+		setPopupInfo(poi);
 	};
 	return (
 		<div>
@@ -51,9 +52,23 @@ const MainMap = () => {
 				mapStyle='mapbox://styles/mapbox/streets-v9'
 				mapboxAccessToken='pk.eyJ1Ijoic3Vud29uZ2EiLCJhIjoiY2w4eWxkNDZ5MGh2dDN3dDV2ZjNma2M2MiJ9.uRRO8IUZTFjCbk7Jn8q3ng'
 			>
-				{pois.map((poi, i) => (
-					<Marker key={i} longitude={poi.geometry.coordinates[0]} latitude={poi.geometry.coordinates[1]} color={'red'}></Marker>
-				))}
+				{pois &&
+					pois.map((poi, i) => (
+						<Marker
+							key={i}
+							longitude={poi.center[0]}
+							latitude={poi.center[1]}
+							color={'red'}
+							onClick={(e, p = poi) => {
+								onClickMarker(e, p);
+							}}
+						></Marker>
+					))}
+				{popupInfo && (
+					<Popup anchor='top' longitude={Number(popupInfo.center[0])} latitude={Number(popupInfo.center[1])} onClose={() => setPopupInfo(null)}>
+						{popupInfo.text}
+					</Popup>
+				)}
 			</Map>
 		</div>
 	);
