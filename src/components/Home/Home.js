88939c994508437e5166e8e41d518d58281e1acd
minseoo <alstj991226@gmail.com>
2022-11-17 20:39:36 +0900

    login

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,17 +2,12 @@ import React from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Home.module.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MainCalendar from '../MainCalendar';
-import MainProgress from '../Progress/MainProgress/MainProgress';
 
 const Home = (props) => {
   return (
-    <Routes>
-      <Route path="/" element={<MainCalendar />} />
-      <Route path="/plant" element={<MainProgress />} />
-    </Routes>
+    <Card className={classes.home}>
+      <h1>Login</h1>
+    </Card>
   );
 };
 
