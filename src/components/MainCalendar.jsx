75d61f8bd2df4676c069d21ea9768dd6a06f88e4
hs-1891014-minseokim <55376275+hs-1891014-minseokim@users.noreply.github.com>
2022-11-22 14:15:20 +0900

    Update MainCalendar.jsx

--- src/components/MainCalendar.jsx
+++ src/components/MainCalendar.jsx
@@ -6,33 +6,55 @@ import 'react-calendar/dist/Calendar.css';
 import './MainCalendar.css';
 import { Link } from 'react-router-dom';
 import MainMap from './Map/MainMap';
+import { RiPlantLine } from 'react-icons/ri';
 
 const MainCalendar = () => {
-	const [date, setDate] = useState(new Date());
-	const [show, setShow] = useState(false);
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
 
-	const handleClose = () => setShow(false);
-	const handleShow = (value, event) => {
-		setDate(value);
-		setShow(true);
-	};
-	return (
-		<>
-			<Calendar onClickDay={handleShow} onChange={setDate} value={date} formatDay={(locale, date) => date.toLocaleDateString('en', { day: 'numeric' })} calendarType='US'></Calendar>
-			<Link className='plantBtn' to={'/plant'} state={{ date: date }}>
-				Plant
-			</Link>
-			<Modal size='xl' show={show} onHide={handleClose} centered>
-				<Modal.Header>
-					<Button onClick={handleClose}>{'<'}</Button>
-					<Modal.Title>{`${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`}</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					<MainTodoList date={date}></MainTodoList>
-					<MainMap></MainMap>
-				</Modal.Body>
-			</Modal>
-		</>
-	);
+  const handleClose = () => setShow(false);
+  const handleShow = (value, event) => {
+    setDate(value);
+    setShow(true);
+  };
+  return (
+    <>
+      <Link className="plantBtn" to={'/plant'} state={{ date: date }}>
+        <RiPlantLine />
+        Plant
+      </Link>
+      <Calendar
+        onClickDay={handleShow}
+        onChange={setDate}
+        value={date}
+        formatDay={(locale, date) =>
+          date.toLocaleDateString('en', { day: 'numeric' })
+        }
+        calendarType="US"
+      ></Calendar>
+      <Modal size="xl" show={show} onHide={handleClose} centered>
+        <Modal.Header>
+          <Button className="buttoncc" onClick={handleClose}>
+            {'<'}
+          </Button>
+          <Modal.Title>{`${date.getFullYear()}.${
+            date.getMonth() + 1
+          }.${date.getDate()}`}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container fluid>
+            <Row>
+              <Col>
+                <MainTodoList date={date}></MainTodoList>
+              </Col>
+              <Col xl="6" md="12">
+                <MainMap></MainMap>
+              </Col>
+            </Row>
+          </Container>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 };
 export default MainCalendar;
