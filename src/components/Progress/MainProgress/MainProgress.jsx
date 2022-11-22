d54b428994c14a91af6e4e5e4acb5477b19ea949
ej-kimm <115481749+ej-kimm@users.noreply.github.com>
2022-11-22 12:33:48 +0900

    css plant 배경색 추가

--- src/components/Progress/MainProgress/MainProgress.jsx
+++ src/components/Progress/MainProgress/MainProgress.jsx
@@ -1,27 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Progress from '../Progress/Progress';
 import { useLocation } from 'react-router-dom';
 import './MainProgress.css';
 import { Container, Stack, Button } from 'react-bootstrap';
-import { BsCalendar3 } from 'react-icons/bs'
+import { BsCalendar3 } from 'react-icons/bs';
 
 const MainProgress = () => {
-    const location = useLocation()
-    const date = location.state.date
-    const month = date.getMonth()+1
-    
-    return (
-        <Container>
-            <Link className='calendarBtn' to={'/'} >
-                    <BsCalendar3/>
-                    Calendar
-            </Link>
-            <div className='background'>
-                <div className='month'>{month}</div>
-                <Progress date={date}/>
-            </div>
-        </Container>      
-    )
+  const location = useLocation();
+  const date = location.state.date;
+  const month = date.getMonth() + 1;
+
+  return (
+    <Container>
+      <Link className="calendarBtn" to={'/'}>
+        <BsCalendar3 />
+        Calendar
+      </Link>
+      <div className="background">
+        <div className="month">{month}</div>
+        <Progress date={date} />
+      </div>
+    </Container>
+  );
 };
 export default MainProgress;
