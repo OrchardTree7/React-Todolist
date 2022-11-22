3a894c6eca688a1913b211a957f814346c127296
hs-1891014-minseokim <55376275+hs-1891014-minseokim@users.noreply.github.com>
2022-11-22 14:15:46 +0900

    Update MainTodoList.jsx

--- src/components/MainTodoList.jsx
+++ src/components/MainTodoList.jsx
@@ -7,24 +7,18 @@ import './MainTodoList.css';
 const filters = ['all', 'active', 'completed'];
 
 const MainTodoList = ({ date }) => {
-  const [filter, setFilter] = useState(filters[0]);
-  const selDate = `${date.getFullYear()}.${
-    date.getMonth() + 1
-  }.${date.getDate()}`;
-  return (
-    <Container>
-      <Form.Group className="conn" controlId="formListItem">
-        <Stack direction="horizontal">
-          <Header
-            filters={filters}
-            filter={filter}
-            onFilterChange={setFilter}
-          />
-          <TodoList filter={filter} selDate={selDate} date={date} />
-        </Stack>
-      </Form.Group>
-    </Container>
-  );
+	const [filter, setFilter] = useState(filters[0]);
+	const selDate = `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()}`;
+	return (
+		<Container>
+			<Form.Group className='conn' controlId='formListItem'>
+				<Stack direction='horizontal' gap={3}>
+					<Header filters={filters} filter={filter} onFilterChange={setFilter} />
+					<TodoList filter={filter} selDate={selDate} date={date} />
+				</Stack>
+			</Form.Group>
+		</Container>
+	);
 };
 
 export default MainTodoList;
